72ed6c67cbec37c648ef2e0cb88dfae06036513a
seema626 <madaneseema36@gmail.com>
2024-11-30 17:11:40 +0530

    added

---
+++ test.py
@@ -0,0 +1,17 @@
+# Simple_intrest calculation
+
+def Simple_intrest(p, n, r):
+    i=(p, n, r)/100
+    amt = p+ i
+    return {"intrest": i,"amount": amt}
+
+# Takeinput from userto console
+p = float(input("please enter principle in INR: "))
+n = float(input("please enter Number of year :" ))
+r = float(input("please enter rate of intrest in %p.a :"))
+
+# Calculate intrest and amount
+results = Simple_intrest(p, n, r)
+
+#print the results
+print(results)
